65f56d551dc4cf0df2eecbff3d8bef228bf9001a
shashidhar078 <nagunurishashidhar@gmail.com>
2025-10-22 19:20:02 +0530

    difference between const and let in counter example

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -1,29 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+  import { useState } from 'react'
+  import reactLogo from './assets/react.svg'
+  import viteLogo from '/vite.svg'
+  import './App.css'
 
-function App() {
-  let [counter, setCount] = useState(15)
-  const changeValue=()=>{
-    console.log("value of counter : ",counter);
-    counter=counter+1;
-    setCount(counter)
-  }
-  const removeValue=()=>{
-    console.log("value of counter : ",counter);
-    counter=counter-1;
-    setCount(counter)
-  }
-  return (
-    <>
-      <h1>Welcome to Soul society</h1>
-      <h2>Value of count : {counter}</h2>
-      <button onClick={changeValue}>Add value</button>
-      <br/>
-      <button onClick={removeValue}>Remove value</button>
-    </>
-  )
-}
+  function App() {
+    let [counter, setCount] = useState(15)
+    //if you use const here counter should be assigned properly
+    //you cannot reassign like counter=counter+1 if you use const
+    //since counter is a const variable
+  //   const [counter, setCount] = useState(15)
 
-export default App
+  // const changeValue = () => {
+  //   console.log("value of counter:", counter)
+  //   setCount(counter + 1)   // ✅ Correct way to update state
+  // }
+
+  // const removeValue = () => {
+  //   console.log("value of counter:", counter)
+  //   setCount(counter - 1)   // ✅ Correct way to update state
+  // }
+    const changeValue=()=>{
+      console.log("value of counter : ",counter);
+      counter=counter+1;
+      setCount(counter)
+    }
+    const removeValue=()=>{
+      console.log("value of counter : ",counter);
+      counter=counter-1;
+      setCount(counter)
+    }
+    return (
+      <>
+        <h1>Welcome to Soul society</h1>
+        <h2>Value of count : {counter}</h2>
+        <button onClick={changeValue}>Add value</button>
+        <br/>
+        <button onClick={removeValue}>Remove value</button>
+      </>
+    )
+  }
+
+  export default App
