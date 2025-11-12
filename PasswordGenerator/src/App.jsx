77534ebfce2a0545392a65e6ad47eb3402ee4c31
shashidhar078 <nagunurishashidhar@gmail.com>
2025-11-12 10:04:48 +0530

    updated styling

--- PasswordGenerator/src/App.jsx
+++ PasswordGenerator/src/App.jsx
@@ -1,7 +1,7 @@
 import React,{ useState,useCallback } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [length,setLength]=useState(0);
@@ -9,33 +9,58 @@ function App() {
   const [charAllowed,setcharAllowed]=useState(false);
   const [password,setPassword]=useState("");
 
-  const passwordGenerator=useCallback(
-    () => {
-      let pass="";
-      let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-      if(numberAllowed)
-      {
-        str+="0123456789"
-      }
-      if(charAllowed)
-      {
-        str+="!@#$%^&*()_+-=[]{}|;:'\",.<>/?`~";
-      }
-      for(let i=1;i<=array.length;i++)
-      {
-        let char=Math.floor(Math.random()*str.length+1);
-        pass=str.charAt(char);
-      }
-      setPassword(pass);
-    },
-    [fn,length,numberAllowed,charAllowed,setPassword],
-  )
+  // const passwordGenerator=useCallback(
+  //   () => {
+  //     let pass="";
+  //     let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  //     if(numberAllowed)
+  //     {
+  //       str+="0123456789"
+  //     }
+  //     if(charAllowed)
+  //     {
+  //       str+="!@#$%^&*()_+-=[]{}|;:'\",.<>/?`~";
+  //     }
+  //     for(let i=1;i<=array.length;i++)
+  //     {
+  //       let char=Math.floor(Math.random()*str.length+1);
+  //       pass=str.charAt(char);
+  //     }
+  //     setPassword(pass);
+  //   },
+  //   [fn,length,numberAllowed,charAllowed,setPassword],
+  // )
   
   return (
     <>
-      <h1 text-4xl text-center text-white>Password Generator</h1>
+      <div className="w-full max-w-lg mx-auto shadow-md rounded-lg px-4 py-3 my-5 bg-gray-800 text-orange-500">
+      <h1 className='text-white text-center my-3'>Password generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+          type="text"
+          id="password"
+          value={password}
+          className="outline-none w-full py-1 px-3"
+          placeholder="Password"
+        />
+        <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+        </div>
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e)=>{setLength(e.target.value)}}
+            / >
+              <label>Length:{length}</label>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
