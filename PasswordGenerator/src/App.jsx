033316c462e6e8f94564ae810aa85bf3343522e5
shashidhar078 <nagunurishashidhar@gmail.com>
2025-11-14 18:26:31 +0530

    implemented copy feature and understood useRef

--- PasswordGenerator/src/App.jsx
+++ PasswordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import React,{ useState,useCallback,useEffect } from 'react'
+import React,{ useState,useCallback,useEffect,useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
@@ -30,9 +30,15 @@ function App() {
     },
     [length,numberAllowed,charAllowed,setPassword],
   )
+  const copyToClipboard=useCallback(()=>{
+    window.navigator.clipboard.writeText(password)
+  },[password])
+
   useEffect(()=>{
     passwordGenerator()
   },[length,numberAllowed,charAllowed,passwordGenerator])
+
+  const passwordRef=useRef(null);
   return (
     <>
       <div className="w-full max-w-lg mx-auto shadow-md rounded-lg px-4 py-3 my-5 bg-gray-800 text-orange-500">
@@ -45,8 +51,9 @@ function App() {
           className="outline-none w-full py-1 px-3"
           placeholder="Password"
           readOnly
+          ref={passwordRef}
         />
-        <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+        <button onClick={copyToClipboard} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 cursor-pointer'>copy</button>
         </div>
         <div className='flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
