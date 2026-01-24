5e73b737ae434864634135645dc44eefff07f999
shashidhar078 <nagunurishashidhar@gmail.com>
2026-01-24 23:53:58 +0530

    implemented todoItem and injected components into App.jsx

--- TodoContext/src/App.jsx
+++ TodoContext/src/App.jsx
@@ -49,9 +49,15 @@ function App() {
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
+                        <TodoForm/>
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo)=>(
+                          <div key={todo.id}>
+                            <TodoItem todo={todo}/>
+                          </div>
+                        ))}
                     </div>
                 </div>
             </div>
--- TodoContext/src/Components/TodoItem.jsx
+++ TodoContext/src/Components/TodoItem.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
-
+import {useState} from 'react'
+import {useTodo} from '../Contexts/index.js'
 function TodoItem({ todo }) {
-    
+    const [isTodoEditable,setIsTodoEditable]=useState(false)
+    const [todoMsg,setTodoMsg]=useState(todo.todo)
+
+    const {updateTodo,deleteTodo,toggleTodo}=useTodo();
+
+    const editTodo=()=>{
+        updateTodo(todo.id,{...todo,todo:todoMsg})
+        setIsTodoEditable(false)
+    }
+    const toggleCompleted=()=>{
+        toggleTodo(todo.id)
+    }
 
     return (
         <div
