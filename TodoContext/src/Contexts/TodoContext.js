bfd74867f75e56b60b668ac27199ccc7e2e269cf
shashidhar078 <nagunurishashidhar@gmail.com>
2026-01-24 18:45:25 +0530

    default values provided in TodoContext and updated file path in index.js

--- TodoContext/src/Contexts/TodoContext.js
+++ TodoContext/src/Contexts/TodoContext.js
@@ -1,6 +1,16 @@
 import {createContext,useContext} from 'react';
 
-export const TodoContext=createContext();
+export const TodoContext=createContext({
+    todos:[{
+        id:1,
+        todo:"Todo msg",
+        completed:false,
+    }],
+    addTodo:(todo)=>{},
+    updateTodo:(id,todo)=>{},
+    deleteTodo:(id)=>{},
+    toggleTodo:(id)=>{},
+});
 
 export function useTodo()
 {
